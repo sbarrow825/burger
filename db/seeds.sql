8a1cd4eb7351d5990058d04adfc39174780e4819
sbarrow825 <66639249+sbarrow825@users.noreply.github.com>
2020-07-25 17:27:02 -0700

    set up MVC structure

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Big-Mac", true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Baconator", false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie Burger", false);
