1ebe10514d96d3fd3f6a3b247308f7617c264025
sbarrow825 <66639249+sbarrow825@users.noreply.github.com>
2020-07-26 21:32:31 -0700

    added styling to the application

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var PORT = process.env.PORT || 3000;
 var app = express();
 
 app.use(express.static("public"));
+app.use(express.static("views/Images"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
